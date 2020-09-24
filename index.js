205cf19f5dc71a3ef8854c8892bfb84a054248ad
Chia Yeng Xiong <chiayengxiong@Chias-MacBook-Pro.local>
2020-09-24 10:49:57 -0500

    Added button to start game in index.html. Added buildPlayer method.

--- index.js
+++ index.js
@@ -1,83 +1,131 @@
 "use strict"
 
-// 1. prompt user for valid result(i.e. rock, paper, scissor)
-// 2. create an array to store value of rock, paper, scisssor
+// 1. prompt user for valid result(i.e. rock, paper, scissor, lizard, spock)
+// 2. create an array to store value of rock, paper, scissor, lizard, spock
 // 3. display which player won the round
 // 4. display winner best of three
 // 5. want human vs AI and human vs human
 
-
-class Game {
-    constructor() {
-        this.playerOne = new Player(prompt("What is the name of Player One?"));
-        this.playerTwo = new Player(prompt("What is the name of Player Two?"));
-
-    }
+function app() {
+  Game = new Game();
+  Game.runGame();
 }
 
-class Player {
-    constructor() {
-        this.score = 0;
-        this.name = null;
+class Game {
+  constructor() {
+  }
 
-        this.gestures = [new Gestures("rock"), new Gestures("paper"), new Gestures("scissor")];
+  runGame() {
+    this.displayRules();
+  }
+
+  buildPlayer() {
+    let numberOfPlayers = prompt("How many do you have?");
+    let playerResults = parseInt(numberOfPlayers);
+
+    if (playerResults === 2) {
+      this.playerOne = new Player(prompt("What is the name of Player One?"));
+      this.playerTwo = new Player(prompt("What is the name of Player Two?"));
     }
+    else if (playerResults === 1) {
+      this.playerOne = new Player(prompt("What is the name of Player One?"));
+      this.playerTwo = new Ai();
+    }
+    return numberOfPlayers;
+  }
+
+  displayRules() {
+    console.log("Welcome to a game of Rock, Paper, Scissors, Lizard, and Spock");
+    console.log("Best of three is the winner");
+  }
+
+  displayWinner() {
+
+  }
+}
+
+//Player class
+class Player {
+  constructor(name) {
+    this.score = 0;
+    this.name = name;
+    this.gesture = "";
+  }
+
+  callGestures() {
+    let gestureInput = promptFor("Input your gesture.", gestureValidation).toLowerCase();
+    this.gesture = gestureInput;
+    console.log("Gesture Result is: " + this.gesture);
+  }
 }
 
 class Human extends Player {
-    constructor(name) {
-        super(name);
-    }
+  constructor(name) {
+    super(name);
+  }
 }
 
 class Ai extends Player {
-    constructor(name) {
-        super(name);
-    }
+  constructor() {
+    super();
+  }
 
+  aiName() {
+    let randomName = ["Abe", "Brittney", "Charles", "Davis", "Edward", "Frankie", "Gina", "Hanna"];
+
+    let choosenName = Math.floor(Math.random(randomName.length) + 1);
+    this.name = new Ai(choosenName);
+  }
 }
 
+// gestures classes
 class Gestures {
-    constructor(name) {
-        this.name = name;
-    }
+  constructor(name) {
+    this.name = name;
+  }
 }
 
 class Rock extends Gestures {
-    constructor(name) {
-        super(name);
-    }
+  constructor(name) {
+    super(name);
+  }
 }
 
-class Paper extends Gesture {
-    constructor(name) {
-        super(name);
-    }
+class Paper extends Gestures {
+  constructor(name) {
+    super(name);
+  }
 }
 
-class Scissor extends Gesture {
-    constructor(name) {
-        super(name);
-    }
+class Scissor extends Gestures {
+  constructor(name) {
+    super(name);
+  }
 }
 
-// use inheritance for different gestures
+class Lizard extends Gestures {
+  constructor(name) {
+    super(name);
+  }
+}
 
-
-
-
-
-
-
-
-
-
-
-
+class Spock extends Gestures {
+  constructor(name) {
+    super(name);
+  }
+}
 
 function promptFor(question, valid) {
-    do {
-        var response = prompt(question).trim();
-    } while (!response || !valid(response));
-    return response;
+  do {
+    var response = prompt(question).trim();
+  } while (!response || !valid(response));
+  return response;
+}
+
+function gestureValidation(input) {
+  return input.toLowerCase() === "rock" ||
+    input.toLowerCase() === "paper" ||
+    input.toLowerCase() === "scissor" ||
+    input.toLowerCase() === "lizard" ||
+    input.toLowerCase() === "spock";
 }
