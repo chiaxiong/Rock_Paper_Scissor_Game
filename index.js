dbd1bac57723881c3500413d9ed26c05213697bd
Chia Yeng Xiong <chiayengxiong@Chias-MacBook-Pro.local>
2020-09-24 15:03:08 -0500

    Added conditional for players. Revising random AI name.

--- index.js
+++ index.js
@@ -13,23 +13,41 @@ function app() {
 
 class Game {
   constructor() {
-  }
-
-  runGame() {
     this.displayRules();
   }
 
-  buildPlayer() {
-    let numberOfPlayers = prompt("How many do you have?");
+  runGame() {
+    let numberOfPlayers = this.buildPlayers();
+    console.log("Welcome " + this.playerOne.getName());
+
+    if (numberOfPlayers === 2) {
+      console.log("Welcome " + this.playerTwo.getName());
+    } else if (numberOfPlayers === 1) {
+      console.log("Welcome " + this.playerAi.getName());
+    }
+
+    this.playerOne.callGestures();
+    console.log(this.playerOne + "gesture is: " + this.playerOne);
+
+    if (numberOfPlayers === 2) {
+      console.log(this.playerTwo + "gesture is: " + this.playerTwo);
+    } else if (numberOfPlayers === 1) {
+      console.log(this.playerAi.getName() + "gesture is: " + this.playerAi);
+    }
+  }
+
+  buildPlayers() {
+    let numberOfPlayers = prompt("How many players do you have?");
     let playerResults = parseInt(numberOfPlayers);
 
+
     if (playerResults === 2) {
-      this.playerOne = new Player(prompt("What is the name of Player One?"));
-      this.playerTwo = new Player(prompt("What is the name of Player Two?"));
+      this.playerOne = new Human(prompt("What is the name of Player One?"));
+      this.playerTwo = new Human(prompt("What is the name of Player Two?"));
     }
     else if (playerResults === 1) {
-      this.playerOne = new Player(prompt("What is the name of Player One?"));
-      this.playerTwo = new Ai();
+      this.playerOne = new Human(prompt("What is the name of Player One?"));
+      this.playerAi = new Ai();
     }
     return numberOfPlayers;
   }
@@ -46,35 +64,57 @@ class Game {
 
 //Player class
 class Player {
-  constructor(name) {
+  constructor() {
     this.score = 0;
-    this.name = name;
+    this.name = null;
     this.gesture = "";
   }
 
   callGestures() {
     let gestureInput = promptFor("Input your gesture.", gestureValidation).toLowerCase();
-    this.gesture = gestureInput;
-    console.log("Gesture Result is: " + this.gesture);
+    switch (gestureInput) {
+      case "rock":
+        this.gesture = new Rock(gestureInput);
+        break;
+      case "scissor":
+        this.gesture = new Scissor(gestureInput);
+        break;
+      case "paper":
+        this.gesture = new Paper(gestureInput);
+        break;
+      case "lizard":
+        this.gesture = new Lizard(gestureInput);
+        break;
+      case "spock":
+        this.gesture = new Spock(gestureInput);
+        break;
+      default:
+        break;
+    }
   }
+
+  getGesture() {
+    return this.gesture;
+  }
+
+  getName() {
+    return this.name;
+  }
+
 }
 
 class Human extends Player {
   constructor(name) {
     super(name);
+    this.name = name;
   }
 }
 
 class Ai extends Player {
   constructor() {
-    super();
-  }
-
-  aiName() {
     let randomName = ["Abe", "Brittney", "Charles", "Davis", "Edward", "Frankie", "Gina", "Hanna"];
-
-    let choosenName = Math.floor(Math.random(randomName.length) + 1);
-    this.name = new Ai(choosenName);
+    let choosenName = Math.floor(Math.random() * randomName.length);
+    super(randomName[choosenName]);
   }
 }
 
@@ -82,6 +122,10 @@ class Ai extends Player {
 class Gestures {
   constructor(name) {
     this.name = name;
+  }
+
+  getGestureName() {
+    return this.name;
   }
 }
 
